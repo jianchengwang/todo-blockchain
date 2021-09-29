341f8138cf09c43ec2690fd5b18f2c8f70241420
jianchengwang <jiancheng_wang@yahoo.com>
2021-09-29 18:14:23 +0800

    test hardhat

--- hardhat-demo/contracts/PerformanceToken.sol
+++ hardhat-demo/contracts/PerformanceToken.sol
@@ -8,6 +8,15 @@ contract Token is ERC20 {
     // An address type variable is used to store ethereum accounts.
     address public owner;
 
+    mapping(uint256 => TaskLedger[]) public taskLedgers;
+
+    struct TaskLedger {
+        uint256 taskId;
+        bytes32 sysNoticeType;
+        bytes32 taskTransactionType;
+        bytes32 txId;
+    }
+
     constructor() ERC20("PerformanceToken", "PTC") {
         _mint(msg.sender, 1000000 * 10 ** decimals());
         owner = msg.sender;
@@ -20,13 +29,14 @@ contract Token is ERC20 {
      * The `external` modifier makes a function *only* callable from outside
      * the contract.
      */
-    function transferTo(address to, uint256 amount) external {
+    function transferTo(address from, address to, uint256 amount) external {
         // Check if the transaction sender has enough tokens.
         // If `require`'s first argument evaluates to `false` then the
         // transaction will revert.
-        require(balanceOf(msg.sender) >= amount, "Not enough tokens");
+        require(msg.sender == owner, "Not owner user");
+        require(balanceOf(from) >= amount, "Not enough tokens");
 
-        console.log("Sender balance is %s tokens", balanceOf(msg.sender));
+        console.log("Sender balance is %s tokens", balanceOf(from));
         console.log("Trying to send %s tokens to %s", amount, to);
 
         // Transfer the amount.
