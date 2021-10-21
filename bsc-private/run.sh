c785742e58cb782d3fff422ba0f8ce2584cf5e94
jianchengwang <jiancheng_wang@yahoo.com>
2021-10-21 16:18:05 +0800

    update bsc-private

---
+++ bsc-private/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo 'Asia/Shanghai' > /etc/timezone/timezone
+
+docker run -itd --restart=unless-stopped -v /etc/localtime:/etc/localtime -v /etc/timezone/timezone:/etc/timezone --name private-bsc -v /opt/docker/bsc-private/:/data -p 30311:30311 -p 8545:8545 -p 8546:8546 private_bsc:v1.1.2 --config config.toml --datadir /data/bsc  --nodiscover --allow-insecure-unlock
