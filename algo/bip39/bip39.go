a1a4a7d9887623dac89fc098e8e25d268102d8d2
jianchengwang <jiancheng_wang@yahoo.com>
2022-04-07 17:36:59 +0800

    feat: add bsc-docker

---
+++ algo/bip39/bip39.go
@@ -0,0 +1,9 @@
+// Package bip39 is the Golang implementation of the BIP39 spec.
+//
+// The official BIP39 spec can be found at
+// https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki
+package bip39
+
+
+
+
---
+++ algo/bip39/go.mod
@@ -0,0 +1,6 @@
+module bip39
+require (
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+	golang.org/x/text v0.3.0
+)
+go 1.17
